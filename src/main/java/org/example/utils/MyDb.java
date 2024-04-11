875aa3ca53a24728a9c18043f0117dd97873c8c0
mohamedxxhechxxbenxxhadid <mohamedhechmi.benhadid@esprit.tn>
2024-04-11 17:44:45 +0100

    "db with javafx"

---
+++ src/main/java/org/example/utils/MyDb.java
@@ -0,0 +1,43 @@
+package org.example.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDb {
+
+    public final String USERNAME = "root";
+    public final String PWD = "";
+    public final String URL = "jdbc:mysql://localhost:3306/pidevSymfony11";
+
+    private Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    //3 creer une variable static pour stocker l'instance
+    public static MyDb instance;
+
+
+    //1 rendre le constructeur privee
+    private MyDb() {
+        try {
+            con = DriverManager.getConnection(URL, USERNAME, PWD);
+            System.out.println("connected to DB");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    //2 creer une methode intermediaire
+    public static MyDb getInstance() {
+        if (instance == null) {
+            instance = new MyDb();
+        }
+        return instance;
+    }
+
+
+}
