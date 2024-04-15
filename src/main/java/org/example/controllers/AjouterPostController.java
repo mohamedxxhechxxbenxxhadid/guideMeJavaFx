09c6d00a8fafe9868a727303500aaf8d817c58ea
3LHECH <mohamedhechmi.benhadid@gmail.com>
2024-04-15 22:57:06 +0100

    "start to add pictures damn u all hahaha "

--- src/main/java/org/example/controllers/AjouterPostController.java
+++ src/main/java/org/example/controllers/AjouterPostController.java
@@ -1,12 +1,18 @@
 package org.example.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.example.models.Post;
@@ -14,25 +20,23 @@ import org.example.models.PostImage;
 import org.example.services.ServicePost;
 import org.example.services.ServicePostImage;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.*;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class AjouterPostController  {
 
-    Set<File> setImages = new HashSet<>(); ;
-    FileChooser fileChooser = new FileChooser();
+    private ArrayList<File> arrayImages= new ArrayList<>(); ;
+    private FileChooser fileChooser = new FileChooser();
+
+    private int sizeListView = 0;
+
+    @FXML
+    private Button RemoveImage;
     @FXML
     private TextArea descriptionId;
 
     @FXML
-    private TextArea imageAreaId;
+    private ListView<String> imageAreaId;
 
     @FXML
     private Button saveId;
@@ -44,14 +48,19 @@ public class AjouterPostController  {
     private Button uploadId;
 
     @FXML
-    void savePost(ActionEvent event) {
+    private ImageView imageUploaded;
+
+    @FXML
+    private HBox imagesHbox;
+
+    public void savePost(javafx.event.ActionEvent actionEvent) {
         Post post = new Post();
         post.setTitle(titleId.getText());
         post.setDescription(descriptionId.getText());
     }
     public void UploadImage(javafx.event.ActionEvent actionEvent) {
+        RemoveImage.setDisable(true);
         String fileExtention ;
-        System.out.println("function works");
         ServicePostImage sPI = new ServicePostImage();
         ServicePost sP = new ServicePost();
         try{
@@ -60,11 +69,32 @@ public class AjouterPostController  {
             if(fileExtention.equals("jpeg") || fileExtention.equals("jpg") || fileExtention.equals("png")){
                 InputStream in = new FileInputStream(file);
                 PostImage pI = new PostImage(sP.findPostById(12),in);
-                imageAreaId.appendText(file+"\n");
-                setImages.add(file);
-                System.out.println(setImages.size());
-                System.out.println(setImages);
-                System.out.println(file);
+                if(arrayImages.contains(file)){
+                    System.out.println("it contains it ");
+                }else{
+                    arrayImages.add(file);
+                    VBox imageBox = new VBox();
+                    Button imageButton = new Button();
+                    imageButton.setText("X");
+                    imageButton.setPrefWidth(20);
+                    imageButton.setPrefHeight(20);
+                    imageBox.setPrefWidth(140);
+                    imageBox.setPrefHeight(140);
+                    ImageView imageShown = new ImageView();
+                    String imagePath = file.toURI().toString();
+                    Image image = new Image(imagePath);
+                    imageShown.setImage(image);
+                    imageShown.setFitWidth(100);
+                    imageShown.setFitHeight(100);
+                    imageBox.getChildren().add(imageButton);
+                    imageBox.getChildren().add(imageShown);
+                    imageButton.setId(file.toString());
+                    imageButton.setOnMouseClicked(event -> this.removePicture(event,imagesHbox,imageBox));
+                    imagesHbox.getChildren().add(imageBox);
+                    //imageAreaId.getItems().add(arrayImages.get(arrayImages.size()-1)+"\n");
+                    //changePicture(file);
+                }
+
             }else{
                 Stage stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
                 showErrorPopup("the file is not an image",stage);
@@ -73,6 +103,52 @@ public class AjouterPostController  {
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+    @FXML
+    public void removePicture(MouseEvent Event,HBox ImagesHbox,VBox imageBox) {
+        System.out.println("boom");
+        ImagesHbox.getChildren().remove(imageBox);
+    }
+    @FXML
+    public void removePicture(ActionEvent event) {
+        int selectedId = imageAreaId.getSelectionModel().getSelectedIndex();
+        imageAreaId.getItems().remove(selectedId);
+        String nameOfFile = imageAreaId.getItems().get(selectedId);
+        for (File fileIt : arrayImages) {
+            if(nameOfFile.equals(fileIt.toString())){
+                arrayImages.remove(fileIt);
+                changePicture(fileIt);
+                break;
+            }
+        }
+        changePicture();
+    }
+    @FXML
+    public void activateRemove(MouseEvent event) {
+        RemoveImage.setDisable(false);
+        changePicture();
+    }
+    public void changePicture(){
+        File file = null;
+        for (File fileIt : arrayImages) {
+            file = fileIt ;
+            break;
+        }
+        if(file!=null){
+            String imagePath = file.toURI().toString();
+            Image image = new Image(imagePath);
+            imageUploaded.setImage(image);
+        }
+
+    }
+
+    public void changePicture(File file){
+        if(file!=null){
+            String imagePath = file.toURI().toString();
+            Image image = new Image(imagePath);
+            imageUploaded.setImage(image);
+        }
+
     }
     public static String getFileExtension(String fullName) {
         String fileName = new File(fullName).getName();
@@ -86,4 +162,6 @@ public class AjouterPostController  {
         alert.setContentText(message);//ww  w . j  a  va2s  .  co  m
         alert.show();
     }
+
+
 }
